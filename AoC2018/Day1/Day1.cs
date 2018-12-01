ef85406d96cd0dc681b30f026316a3514b7cb4e4
Piotr Nowicki <piootrek.nowicki@gmail.com>
2018-12-01 22:50:38 +0100

    Add project files.

---
+++ AoC2018/Day1/Day1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace AoC2018.Day1
+{
+    class DayOne
+    {
+        int frequency;
+        bool duplicate_frequency_found;
+        List<string> input;
+        List<int> list_of_frequencies;
+
+        public DayOne()
+        {
+            this.frequency = 0;
+            this.duplicate_frequency_found = false;
+            this.list_of_frequencies = new List<int>
+            {
+                this.frequency
+            };
+            this.input = File.ReadAllLines("D://Projekty//AoC2018//AoC2018//Day1//Day1.txt").ToList();
+        }
+
+        public void CalculateResult1()
+        {
+            for(int i = 0; i < this.input.Count; i++)
+            {
+                this.ModifyFrequency(this.input[i]);
+            }
+            System.Console.WriteLine("The result of Day1 part one is " + this.frequency);
+        }
+
+        public void CalculateResult2()
+        {
+            while (this.duplicate_frequency_found == false)
+            {
+                for (int i = 0; i < this.input.Count; i++)
+                {
+                    this.ModifyFrequency(this.input[i]);
+                    for (int j = 0; j < this.list_of_frequencies.Count; j++)
+                    {
+                        if ((this.list_of_frequencies[j] == this.frequency) && (this.duplicate_frequency_found is false))
+                        {
+                            Console.WriteLine("FOUND DUPLICATE " + this.frequency.ToString());
+                            this.duplicate_frequency_found = true;
+                            break;
+                        }
+                    }
+                    this.list_of_frequencies.Add(this.frequency);
+                }
+            }    
+        }
+
+        private void ModifyFrequency(string instruction)
+        {
+            if (instruction[0] == '-')
+            {
+                int number = Int32.Parse(instruction.Substring(1));
+                this.frequency -= number;
+            }
+            if (instruction[0] == '+')
+            {
+                int number = Int32.Parse(instruction.Substring(1));
+                this.frequency += number;
+            }
+        }
+    }
+}
---
+++ AoC2018/Program.cs
@@ -0,0 +1,18 @@
+﻿using AoC2018.Day1;
+using System;
+
+namespace AoC2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOne ResultDay1 = new DayOne();
+            ResultDay1.CalculateResult1();
+            Console.ReadLine();
+            DayOne ResultDay1_1 = new DayOne();
+            ResultDay1_1.CalculateResult2();
+            Console.ReadLine();
+        }
+    }
+}
