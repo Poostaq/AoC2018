bb7374d8a788b5050506b41ecf11a48bd377bf20
Piotr Nowicki <piootrek.nowicki@gmail.com>
2018-12-02 10:24:25 +0100

    Stage Day2 changes

---
+++ AoC2018/Day2/Day2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace AoC2018.Day2
+{
+    class DayTwo
+    {
+        List<String> box_ids;
+        public DayTwo()
+        {
+           this.box_ids = File.ReadAllLines("D://Projekty//AoC2018//AoC2018//Day2//Day2.txt").ToList();
+        }
+
+        public void CalculateResult1()
+        {
+            int checksum = 0;
+            int doubles = 0;
+            int triples = 0;
+            Console.WriteLine("Found checksum is");
+        }
+
+        public void CalculateDoubles()
+        {
+
+        }
+    }
+}
